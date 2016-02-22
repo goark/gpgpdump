267e97c0da381824c46233eb65b1f8982b7644f7
Spiegel <spiegel.im.spiegel@gmail.com>
2016-02-22 16:49:59 +0900

    add copyright.

--- internal/parse/packets/s2k/s2k.go
+++ internal/parse/packets/s2k/s2k.go
@@ -132,3 +132,18 @@ func (s *S2K) getCount() (*items.Item, error) {
 	count := (uint32(16) + (uint32(c) & 0x0f)) << ((uint32(c) >> 4) + EXPBIAS)
 	return items.NewItem("Count", strconv.Itoa(int(count)), fmt.Sprintf("coded: 0x%02x", c), ""), nil
 }
+
+/* Copyright 2016 Spiegel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
