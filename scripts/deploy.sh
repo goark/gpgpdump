d2a8bdfdb2aa3cb8b59fc080b99694f874a1844a
Spiegel <spiegel.im.spiegel@gmail.com>
2017-11-07 19:29:20 +0900

    Delete obsolete files

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -z "${TRAVIS_TAG}" ] ; then exit 0; fi
-gox -output "goxdist/{{.OS}}_{{.Arch}}_${TRAVIS_TAG}/{{.Dir}}" -ldflags "-X main.Version=${TRAVIS_TAG}"
-sh scripts/package.sh
-ghr --username spiegel-im-spiegel --token $GITHUB_TOKEN ${TRAVIS_TAG} goxdist/dist/
