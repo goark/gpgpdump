26aecf1419fbc20f10e248379d62b61702524801
Spiegel <spiegel.im.spiegel@gmail.com>
2020-12-13 10:51:36 +0900

    Update linter option

--- test-all.sh
+++ test-all.sh
@@ -2,6 +2,6 @@
 go mod verify || exit 1
 go mod tidy -v || exit 1
 depm list --json | nancy sleuth -n || exit 1
-golangci-lint run ./... || exit 1
+golangci-lint run --enable gosec ./... || exit 1
 go test ./... || exit 1
 depm m --dot --dot-config dot-config.toml | dot -Tpng -o dependency.png
